1463b95bd44f75ea519475ac8a3ad01f4035824e
mmaddex <mmaddex@render.com>
2026-03-27 15:19:01 -0600

    webserver

---
+++ hello-world/server.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+if __name__ == "__main__":
+    HTTPServer(("", 8080), Handler).serve_forever()
